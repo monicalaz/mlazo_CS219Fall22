17ed9ad4e30ea061c09033c2539938fdf20ec730
monicalazaro <monicalazaro1208@gmail.com>
2022-08-30 14:59:02 -0400

    new changes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,5 +5,9 @@ public class HelloWorld {
     public static void main(String [] args) {
         System.out.println("Hello World");
 
+        int x = 5;
+        boolean z =;
+        double pi = 3.14159;
+
     }
 }
