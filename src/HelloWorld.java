426705299fc9efe43f0725a3fae432c880807b83
monicalazaro <monicalazaro1208@gmail.com>
2022-08-30 15:14:03 -0400

    new changes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,12 +2,22 @@
 
 public class HelloWorld {
 
+    /*
+
+    this is a multiline bock comment
+    Another comment line
+     */
+
     public static void main(String [] args) {
         System.out.println("Hello World");
 
         int x = 5;
         boolean z =;
         double pi = 3.14159;
+        String name = "CS 219 ";
+
+        double avagadros = 6.022140857e23; // Scientific notation
+        double electron_mass = 9.10938357015e-31
 
     }
 }
