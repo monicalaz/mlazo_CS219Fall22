369f06df5c5de0b37f58e12f3571aa250946cb93
monicalazaro <monicalazaro1208@gmail.com>
2022-09-01 15:48:21 -0400

    use packages and new examples

--- src/utility/Investment.java
+++ src/utility/Investment.java
@@ -1,3 +1,5 @@
+package utility;
+
 public class Investment {
 
     public static double investment (double c, double r, int t, int n) {
@@ -12,7 +14,6 @@ public class Investment {
       //  int n = (int) 2e9;  // compound billion times
         // convert the int to a double
     // double p = c * Math.pow( 1 + r/n, t*n );
-        public static void main(String [] args){
-        System.out.println(investment(1000, 0.03,10,365) );
+        public static void main(String [] args){System.out.println(investment(1000, 0.03,10,365) );
     }
 }
